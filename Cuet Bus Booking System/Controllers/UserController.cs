1ce75c70e6cee9a9c95c4522a2f3e5e6fe773f5d
Ahsan Akib <ahsanakib3567@gmailcom>
2025-11-15 16:10:46 +0600

    Add changes for user panel

---
+++ Cuet Bus Booking System/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Cuet_Bus_Booking_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cuet_Bus_Booking_System.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult userIndex()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
